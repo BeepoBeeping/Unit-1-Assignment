d48c16097e7dcb062c43d68880323ec35d33c764
BeepoBeeping <561749@escg.ac.uk>
2025-10-31 15:21:32 +0000

    finished
    finish

--- Assets/Scripts/CollectibleScript.cs
+++ Assets/Scripts/CollectibleScript.cs
@@ -1,21 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class CollectibleScript : MonoBehaviour
 {
+
+    public int coins;
+    public TextMeshProUGUI collectibleText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        coins = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        collectibleText.text = coins.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(gameObject); // destroy game object
+
+        coins = coins + 1;
     }
 }
